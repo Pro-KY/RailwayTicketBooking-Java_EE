86f753aadb39dc2c0fe2d91e5b7f1d90106d22c3
Yuriy <klyuyko.yu@gmail.com>
2019-11-04 16:47:37 +0200

    add URlBuilder

---
+++ src/main/java/com/proky/booking/util/URLBuilder.java
@@ -0,0 +1,44 @@
+package com.proky.booking.util;
+import java.util.Map;
+
+import static com.proky.booking.util.constans.Command.REDIRECT;
+
+public class URLBuilder {
+    private StringBuilder sb = new StringBuilder();
+    private String viewPath;
+    private Map<String, String> parameters;
+    private boolean redirect;
+
+    public URLBuilder(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public URLBuilder(String viewPath, Map<String, String> parameters) {
+        this.viewPath = viewPath;
+        this.parameters = parameters;
+    }
+
+    public URLBuilder(boolean redirect, String viewPath, Map<String, String> parameters) {
+        this.viewPath = viewPath;
+        this.parameters = parameters;
+        this.redirect = redirect;
+    }
+
+    public String buildURL() {
+        if (redirect) {
+            sb.append(REDIRECT);
+        }
+
+        sb.append(viewPath);
+
+        if (parameters != null) {
+            sb.append("?");
+
+            parameters.forEach((parameter,value) -> {
+                sb.append(parameter).append("=").append(value);
+            });
+        }
+
+        return sb.toString();
+    }
+}

432c3ebdd243c8936b5c33adb45a57afc5c97fe4
Yuriy <klyuyko.yu@gmail.com>
2019-11-04 14:35:20 +0200

    add FrontController

---
+++ src/main/java/com/proky/booking/presentation/FrontController.java
@@ -0,0 +1,25 @@
+package com.proky.booking.presentation;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FrontController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+        handleRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost");
+        handleRequest(req, resp);
+    }
+
+    private void handleRequest (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // stub
+    }
+}
